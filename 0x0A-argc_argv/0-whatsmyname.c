d06fef854ffaeda5cfe55b1a5325c453f8cc2537
K-Musty <kmustapha9564@gmail.com>
2023-10-27 20:58:12 -0700

    yeahh

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main method
+ * @argc: number of arguments
+ * @argv: pointers to arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%s\n", argv[0]);
+	return (0);
+}
