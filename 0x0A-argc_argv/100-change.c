6be74d1a9392c8fb401272b20d72f7e3458d9bbf
K-Musty <kmustapha9564@gmail.com>
2023-10-27 21:44:32 -0700

    argc & argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - check the code for Holberton School students.
- * @argc: The size of the argv array.
- * @argv: An array containing the program command line arguments
+ * main - main method
+ * @argc: number of arguments
+ * @argv: pointers to arguments
  *
  * Return: Always 0 || 1.
  */
