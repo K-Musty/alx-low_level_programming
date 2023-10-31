341d5ec55967d83c5d8fba4e6131f40fa5a97c9e
K-Musty <kmustapha9564@gmail.com>
2023-10-30 21:48:34 -0700

    1st try

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,29 +4,78 @@
 #include <string.h>
 
 /**
- * strtow -  splits a string into words.
- * @str: String
+ * countWords - counts words
+ * @str: string
  *
- * Return: strings as words
+ * Return: word count
+ */
+int countWords(const char *str)
+{
+       	int wordCount = 0;
+	int inWord = 0;  
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			inWord = 0;
+		}	
+		else if (inWord == 0)
+		{
+		       	wordCount++;
+			inWord = 1;
+		}
+	}
+	return wordCount;
+}
+
+/**
+ * freee - Helper function to free the memory of a string array
+ *
+ */
+void freee(char **array)
+{
+	if (array)
+	{
+		for (int i = 0; array[i] != NULL; i++)
+		{
+			free(array[i]);
+		}
+		free(array);
+	}
+}
+/**
+ * strtow - splits sentence into words
+ * @str: the sentence string
+ *
+ * Return: pointer to string array
  */
 char **strtow(char *str)
 {
-	char *a = NULL;
-	unsigned int i = 0, j = 0, k;
-
-	if (strncmp(str, "", 1) || str == NULL)
-		return (NULL);
-	a = malloc((i + j + 1) * sizeof(char));
-	if (a == NULL)
-		return (NULL);
-	for (k = 0; k < i; k++)
-		a[k] = str[k];
-	i = k;
-	for (k = 0; k < j; k++)
+	if (str == NULL || *str == '\0')
 	{
-		a[i] = str[k];
-		i++;
+		return NULL;
 	}
-	a[i] = 0;
-	return (NULL);
+       	int wordCount = countWords(str)
+	       	char **words = (char **)malloc((wordCount + 1) * sizeof(char *));
+	if (words == NULL)
+	{
+		return NULL;
+	}
+
+	int wordIndex = 0;
+	char *word = strtok(str, " ");
+	while (word != NULL)
+	{
+		words[wordIndex] = strdup(word);
+		if (words[wordIndex] == NULL)
+		{
+			freee(words);
+			return (NULL);
+		}
+		word = strtok(NULL, " ");
+		wordIndex++;
+	}
+	words[wordIndex] = NULL;
+	return words;
 }
