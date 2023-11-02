9fc59667d86de484ae6bd5e34d5b76f0a5bbc76b
K-Musty <kmustapha9564@gmail.com>
2023-11-01 19:19:50 -0700

    abba

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - main method
  *
  * Return: Always 0
  */
 int main(void)
 {
 	puts("with proper grammar, but the outcome is a piece of art,)\n");
+	printf("i am abba");
 	return (0);
 }
