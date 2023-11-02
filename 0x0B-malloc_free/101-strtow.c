4b9b13d8c594b63e0fbc2ce01c1fd079d2d1ac94
K-Musty <kmustapha9564@gmail.com>
2023-11-01 18:43:10 -0700

    malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,17 +12,17 @@
 int countWords(const char *str)
 {
        	int wordCount = 0;
-	int inWord = 0;  
-
+	int inWord = 0;
+	
 	for (int i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == ' ')
 		{
 			inWord = 0;
-		}	
+		}
 		else if (inWord == 0)
 		{
-		       	wordCount++;
+			wordCount++;
 			inWord = 1;
 		}
 	}
@@ -44,6 +44,7 @@ void freee(char **array)
 		free(array);
 	}
 }
+
 /**
  * strtow - splits sentence into words
  * @str: the sentence string
@@ -54,17 +55,19 @@ char **strtow(char *str)
 {
 	if (str == NULL || *str == '\0')
 	{
-		return NULL;
-	}
-       	int wordCount = countWords(str)
-	       	char **words = (char **)malloc((wordCount + 1) * sizeof(char *));
-	if (words == NULL)
+		return (NULL);
+	
+		int wordCount = countWords(str);
+		char **words = (char **)malloc((wordCount + 1) * sizeof(char *));
+		
+		if (words == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	int wordIndex = 0;
 	char *word = strtok(str, " ");
+
 	while (word != NULL)
 	{
 		words[wordIndex] = strdup(word);
