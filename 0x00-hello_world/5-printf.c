630ffe14cd378c6eebd26c1de85b1eda026ca5ba
K-Musty <kmustapha9564@gmail.com>
2023-11-01 19:14:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-        puts("with proper grammar, but the outcome is a piece of art,)\n";
-        return (0);
-}       
-~               
+	puts("with proper grammar, but the outcome is a piece of art,)\n");
+			return (0);
+}
