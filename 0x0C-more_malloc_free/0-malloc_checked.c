f04f57d7f3d21dc925b34b887f1e4067a561e348
K-Musty <kmustapha9564@gmail.com>
2023-11-02 17:12:49 -0700

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Checks memry size of data
+ * @b: integer
+ *
+ * Return: memory allocated
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
