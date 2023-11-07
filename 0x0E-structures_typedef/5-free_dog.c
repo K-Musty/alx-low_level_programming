5fc1b927f3f0508220a198cf1c6616fcea38d235
K-Musty <kmustapha9564@gmail.com>
2023-11-06 19:43:56 -0800

    commit message

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,4 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * free_dog - lets dog and frees it
+ * @d: the dog
+ *
+ * Return: void/nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
