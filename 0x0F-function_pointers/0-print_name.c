3ef8a7263116fd281701bb4cd2e6e5dfa3a620b8
K-Musty <kmustapha9564@gmail.com>
2023-11-08 16:58:19 -0800

    commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints name
+ * @name: the name to be
+ * @f: function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
