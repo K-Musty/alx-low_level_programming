be2dca5c6b6072f5fc73c68803d0cb929eb42055
K-Musty <kmustapha9564@gmail.com>
2023-11-16 16:10:17 -0800

    commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,14 +4,15 @@
  * free_list - frees a list.
  * @head: pointer
  */
-void free_list(list_t *head)
+ void free_list(list_t *head)
 {
-	list_t *real_node;
+	list_t *later_node;
 
-	while ((real_node = head) != NULL)
+	while ((later_node = head) != NULL)
 	{
 		head = (*head).next;
-		free((*real_node).str);
-		free(real_node);
+		free((*later_node).str);
+		free(later_node);
 	}
 }
+
