5714df6bb78eda3f94492e0e2de93df7cb46ce00
K-Musty <kmustapha9564@gmail.com>
2023-11-21 17:55:10 -0800

    update

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,25 +8,25 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-        listint_t *kalli, *kadi;
+	listint_t *kalli, *kadi;
 
-        if (!head)
-                return (NULL);
-        kalli = kadi = head;
-        while (kadi->next && kadi->next->next)
-        {
-                kadi = kadi->next->next;
-                kalli = kalli->next;
-                if (kadi == kalli)
-                {
-                        kalli = head;
-                        while (kalli != kadi)
-                        {
-                                kalli = kalli->next;
-                                kadi = kadi->next;
-                        }
-                        return (kadi);
-                }
-        }
-        return (NULL);
+	if (!head)
+		return (NULL);
+	kalli = kadi = head;
+	while (kadi->next && kadi->next->next)
+	{
+		kadi = kadi->next->next;
+		kalli = kalli->next;
+		if (kadi == kalli)
+		{
+			kalli = head;
+			while (kalli != kadi)
+			{
+				kalli = kalli->next;
+				kadi = kadi->next;
+			}
+			return (kadi);
+		}
+	}
+	return (NULL);
 }
