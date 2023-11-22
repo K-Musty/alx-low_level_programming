14859c84c328f8cd9559d34e9866405e5892d0c8
K-Musty <kmustapha9564@gmail.com>
2023-11-21 17:50:18 -0800

    103

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop
+ * @head: pointer address
+ *
+ * Return: NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+        listint_t *kalli, *kadi;
+
+        if (!head)
+                return (NULL);
+        kalli = kadi = head;
+        while (kadi->next && kadi->next->next)
+        {
+                kadi = kadi->next->next;
+                kalli = kalli->next;
+                if (kadi == kalli)
+                {
+                        kalli = head;
+                        while (kalli != kadi)
+                        {
+                                kalli = kalli->next;
+                                kadi = kadi->next;
+                        }
+                        return (kadi);
+                }
+        }
+        return (NULL);
+}
