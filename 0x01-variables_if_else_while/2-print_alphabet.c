f6742a1d4d92526ca1d01c8c3e9982e53ca0adb9
K-Musty <kmustapha9564@gmail.com>
2023-11-27 14:31:20 -0800

    0x15-file_io

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
 /**
- * * main - Prints the alphabet.
- * *
- * * Return Always 0 (Success)
- * */
+ * main - Main method
+ *
+ * Return Always 0 (Success)
+ */
 int main(void)
-{	
+{
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz", int i;
-	
+
 	for (i = 0; i < 26; i++)
-	{
 		putchar(alp[i]);
-        }
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
-
-
