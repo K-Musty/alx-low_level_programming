57ce4093679043e761fe4be21ed29fa166711e85
K-Musty <kmustapha9564@gmail.com>
2024-01-17 12:48:41 -0800

    cool

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _isdigit - This will check for digits 0 throuch 9
+ * @c: The character to check
+ * Return: 1 if it is digit otherwse 0
+ */
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
