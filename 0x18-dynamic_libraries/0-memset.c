48b900341e469ec8f640aa43f6910d6060b35f6c
K-Musty <kmustapha9564@gmail.com>
2024-01-17 12:20:01 -0800

    memset.c

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "mainn.h"
+/**
+ * _memset - Function that fills memory bytes
+ * @s: string
+ * @b: b constant byte.
+ * @n: n bytes to fill memory.
+ *
+ * Return: pointer.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+		s[k] = b;
+
+	return (s);
+}
