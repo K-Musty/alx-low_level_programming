0e1c4d4aa52cbf98cfd61b6f4b52e667f200b466
K-Musty <kmustapha9564@gmail.com>
2024-01-17 13:20:20 -0800

    commit

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs -computes the absolute value integer
+ * @n: the int to be computed
+ *
+ * Return: the sbsolut value to the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
